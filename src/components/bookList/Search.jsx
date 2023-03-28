85d3b7ce6827c76ef47052415f157fc640440acd
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-03-28 12:19:49 +0530

    Category page re-usable components logic

--- src/components/bookList/Search.jsx
+++ src/components/bookList/Search.jsx
@@ -4,7 +4,7 @@ import { getAllBooks } from "../../services/book/book.service";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Button } from "semantic-ui-react";
-import '../../assets/styles/App.css'
+import "../../assets/styles/App.css";
 
 const Search = () => {
   const [searchBook, setSearchBook] = useState([]);
@@ -20,35 +20,55 @@ const Search = () => {
       })
       .catch((err) => console.log(err));
   };
+  const handleClick = (id) => {
+    navigate("/category/books/:category/" + id);
+  };
 
   useEffect(() => {
     laodData();
   }, []);
   return (
     <div className="posts">
-      {searchBook
-        .filter((value) => {
-          if (search == "") {
-            return value;
-          } else if (
-            value.title.toLowerCase().includes(search.toLowerCase()) ||
-            value.AuthorName.toLowerCase().includes(search.toLowerCase())
-          ) {
-            return value;
-          }
-        })
-        .map((post) => (
-          <div className="card" key={post.id}>
-            <p>{post.id}</p>
-            <p>{post.title}</p>
-            <p>{post.AuthorName}</p> 
-            <div >
-            <img src={post.Image} alt="image"
-              style={{width : "15em" , height : "20em"  , borderRadius : "5px"}}
-            />
+      <div className="ui">
+        <div className="ui link cards">
+          {searchBook
+            .filter((value) => {
+              if (search == "") {
+                return value;
+              } else if (
+                value.title.toLowerCase().includes(search.toLowerCase()) ||
+                value.AuthorName.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((item) => (
+              <div className="card" key={item.id}>
+              <div className="image">
+                <img
+                  src={item.Image}
+                  alt=""
+                  onClick={() => handleClick(item.id)}
+                />
+              </div>
+              <div className="content">
+                <div className="header">{item.title}</div>
+                <div className="meta">
+                  <p>"{item.AuthorName}"</p>
+                </div>
+                <div className="description">
+                  <b>Price</b>: {item.price}
+                </div>
+              </div>
+              <div className="extra content">
+                <span className="floated">
+                  <b>Book ID : {item.id}</b>
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
