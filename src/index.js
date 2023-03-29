9a8b0bf6276357fd072af8ab7be67e0c055adc4c
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-03-29 12:46:28 +0530

    bug fixing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
+
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
