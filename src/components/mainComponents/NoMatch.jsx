697b9d665fd1568a7bfd457d3456068a695ba7c1
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-04-04 13:21:11 +0530

    Implementation update

--- src/components/mainComponents/NoMatch.jsx
+++ src/components/mainComponents/NoMatch.jsx
@@ -5,7 +5,7 @@ const NoMatch = () => {
     <div className="Image">
       <img
         src="https://cdn.dribbble.com/users/2480087/screenshots/7009361/media/5be4690e38762fd53647912018e86189.gif"
-        alt=""
+        alt="Image"
       />
     </div>
   );
