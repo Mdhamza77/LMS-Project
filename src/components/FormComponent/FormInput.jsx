c28ce85659fd500f98f1ac1cba906b392183bd21
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-04-23 18:03:05 +0530

    checking proptype

--- src/components/FormComponent/FormInput.jsx
+++ src/components/FormComponent/FormInput.jsx
@@ -40,7 +40,7 @@ const FormInput = ({
     name: PropTypes.string.isRequired,
     type: PropTypes.string,
     placeholder: PropTypes.string.isRequired,
-    type: PropTypes.oneOf(['text', 'number', 'password']),
+    type: PropTypes.oneOf(['text', 'number', 'password','email']),
     className: PropTypes.string,
     value: PropTypes.any,
     onChange: PropTypes.func.isRequired
