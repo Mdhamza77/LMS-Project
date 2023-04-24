db8734eaf01f5f64414965f43138ff0c3c30e00f
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-04-24 17:24:18 +0530

    implementation update , production build

--- src/components/user/pages/rentList/RentList.jsx
+++ src/components/user/pages/rentList/RentList.jsx
@@ -83,7 +83,7 @@ const RentList = () => {
             </div>
           ))
       ) : (
-        <div>No data Found</div>
+        <div><img src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg" alt="no data" /></div>
       )}
     </div>
   );
