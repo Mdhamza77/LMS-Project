93da0a278264db607503b9363a356ffca4ff01f3
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-04-26 15:09:30 +0530

    styling component

--- src/components/user/pages/rentList/RentList.jsx
+++ src/components/user/pages/rentList/RentList.jsx
@@ -92,6 +92,7 @@ const RentList = () => {
           <img
             src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg"
             alt="no data"
+            style={{marginTop : "5em" , marginLeft : "20em" , marginRight : "4em" , width : "60em" , height : "40em"}}
           />
         </div>
       )}
